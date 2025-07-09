b02198f89d116ddfb35ba80706f5b263618cc1af
sonnt210 <sonnt.code@gmail.com>
2025-07-09 16:46:22 +0700

    git commit -m "first commit"

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <the-header title="RememberMe"></the-header>
+    <stored-resource :resources="storeResources"></stored-resource>
+</template>
+
+<script>
+import StoredResource from './components/learning-resources/StoredResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+    components: {
+        StoredResource,
+        TheHeader
+    },
+    data() {
+        return {
+            storeResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to Google...',
+                    link: 'https://google.com'
+                },                
+            ]
+        };
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
